8016dab57674b0d6497ea5837896f77b1c6b309a
Evboys <128623554+Evboys@users.noreply.github.com>
2026-03-04 11:57:39 +0100

    Update : Regroupement des errors lors du passage du validator, pour eviter un retour trop long

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -1,16 +1,25 @@
 import { validationResult } from "express-validator";
 
 export const validate = (req, res, next) => {
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            errors: errors.array().map(err => ({
-                field: err.path,
-                message: err.msg
-            }))
-        });
-    }
+  if (!errors.isEmpty()) {
+    const groupedErrors = {};
 
-    next();
+    errors.array().forEach(err => {
+      const field = err.path;
+
+      if (!groupedErrors[field]) {
+        groupedErrors[field] = [];
+      }
+
+      groupedErrors[field].push(err.msg);
+    });
+
+    return res.status(400).json({
+      errors: groupedErrors,
+    });
+  }
+
+  next();
 };
